39e85e04fb74ffe125a5918f21d2379688bbe4c0
Dongwoo Kim <dongwookim@192.168.nate.com>
2022-07-10 18:45:31 +0900

    feat(frog): I added a frog image and text

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { ScrollView } from 'react-native-web';
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      
+      <View style={styles.container}>
+      <Image style={styles.image} source={require('./image/frog.png')}/>
+        <Text>Hello I am a Frog!</Text>
+        <StatusBar style="auto" />
+        
+      </View>
+    </ScrollView>
+
   );
 }
 
@@ -17,4 +26,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image:{
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width:200,
+    height:200,
+  }
 });
