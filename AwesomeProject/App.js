3a90a447c5b954c6385770723fef68e73a1d3364
Dongwoo Kim <dongwookim@Dongwooui-MacBookPro.local>
2022-07-13 21:13:46 +0900

    refactor(App.js): imported React from 'react'

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,8 +1,10 @@
-import { NavigationContainer } from '@react-navigation/native';
+import { NavigationContainer, TabActions } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
-import { ScrollView } from 'react-native-web';
+import { ScrollView, Tab } from 'react-native-web';
+import React from 'react';
+
 
 const Stack=createNativeStackNavigator();
 
@@ -20,27 +22,36 @@ const ProfileScreen = ({ navigation, route }) => {
   return <Text>This is {route.params.name}'s profile</Text>;
 };
 
+function Home(){
+  return(
+    <Tab.Navigator>
+      <Tab.Screen name='Feed' component={Feed}/>
+      <Tab.Screen name='Messages' component={Messages}/>
+    </Tab.Navigator>
+  );
+}
+
 export default function App() {
   return (
     <ScrollView>
       <View style={styles.container}>
         <Image style={styles.image} source={require('./image/frog.png')}/>
         <Text>Hello I am a Frog!</Text>
-        <StatusBar style="auto" />
+      
         
       </View>
       <NavigationContainer>
         <Stack.Navigator>
           <Stack.Screen
            name="Home"
-           component={HomeScreen}
-           options={{title: 'Welcome'}}/>
-           <Stack.Screen
-           name="Profile"
-           component={ProfileScreen}
-           />
+           component={Home}
+           options={{headerShown: false}}/>
+          {/* <Stack.Screen name='Profile' component={Profile}/>
+          <Stack.Screen name='Settings' component={Settings}/>  */}
         </Stack.Navigator>
       </NavigationContainer>
+      
+      
     </ScrollView>
     
   );
@@ -60,5 +71,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     width:200,
     height:200,
+  },
+  button:{
+    padding: 10,
+    height: 400,
+    width: 200
+
   }
+
 });
